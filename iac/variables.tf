edd77290511ad06c8d1bc0987c3b1ad3f06428e2
sukreshmanda <mandasukresh@gmail.com>
2023-06-06 16:31:07 +0530

    Remove unused variables

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,10 +1,6 @@
 variable "AWS_REGION" {
   default = "ap-south-1"
 }
-variable "AWS_AVAILABILITY_ZONES" {
-  type    = set(string)
-  default = ["ap-south-1a", "ap-south-1b"]
-}
 variable "SUBNET_SIZE" {
   description = "size of each subnet"
   default     = 8
