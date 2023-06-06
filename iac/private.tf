d16dcda8aee35293328d4da734a6d0baecc2db19
sukreshmanda <mandasukresh@gmail.com>
2023-06-06 11:18:12 +0530

    Add files for IAc

---
+++ iac/private.tf
@@ -0,0 +1,62 @@
+resource "aws_subnet" "pet-clinic-private-subnet" {
+  vpc_id                  = aws_vpc.pet-clinic-vpc.id
+  availability_zone       = "ap-south-1a"
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = false
+  tags                    = {
+    Name    = "pet-clinic-private-subnet"
+    Creator = "Sukresh"
+  }
+}
+resource "aws_subnet" "pet-clinic-db-subnet" {
+  vpc_id                  = aws_vpc.pet-clinic-vpc.id
+  availability_zone       = "ap-south-1a"
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = false
+  tags                    = {
+    Name    = "pet-clinic-db-subnet"
+    Creator = "Sukresh"
+  }
+}
+
+resource "aws_eip" "pet-clinic-nat-eip" {
+  domain = "vpc"
+  tags   = {
+    Name    = "pet-clinic-nat-eip"
+    Creator = "Sukresh"
+  }
+}
+
+resource "aws_nat_gateway" "pet-clinic-nat-gateway" {
+  subnet_id     = aws_subnet.pet-clinic-public-subnet.id
+  allocation_id = aws_eip.pet-clinic-nat-eip.id
+  depends_on    = [
+    aws_internet_gateway.pet-clinic-ig
+  ]
+  tags = {
+    Name    = "pet-clinic-nat-gateway"
+    Creator = "Sukresh"
+  }
+}
+
+resource "aws_route_table" "pet-clinic-private" {
+  vpc_id = aws_vpc.pet-clinic-vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.pet-clinic-nat-gateway.id
+  }
+  tags = {
+    Name    = "pet-clinic-private"
+    Creator = "Sukresh"
+  }
+}
+
+resource "aws_route_table_association" "pet-clinic-private-subnet-association" {
+  route_table_id = aws_route_table.pet-clinic-private.id
+  subnet_id      = aws_subnet.pet-clinic-private-subnet.id
+}
+
+resource "aws_route_table_association" "pet-clinic-db-subnet-association" {
+  route_table_id = aws_route_table.pet-clinic-private.id
+  subnet_id      = aws_subnet.pet-clinic-db-subnet.id
+}
---
+++ iac/public.tf
@@ -0,0 +1,45 @@
+resource "aws_vpc" "pet-clinic-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = {
+    Name    = "pet-clinic-vpc"
+    Creator = "Sukresh"
+  }
+}
+
+resource "aws_subnet" "pet-clinic-public-subnet" {
+  vpc_id                  = aws_vpc.pet-clinic-vpc.id
+  availability_zone       = "ap-south-1a"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags                    = {
+    Name    = "pet-clinic-public-subnet"
+    Creator = "Sukresh"
+  }
+}
+
+resource "aws_internet_gateway" "pet-clinic-ig" {
+  vpc_id = aws_vpc.pet-clinic-vpc.id
+  tags   = {
+    Name    = "pet-clinic-ig"
+    Creator = "Sukresh"
+  }
+}
+
+resource "aws_route_table" "pet-clinic-public" {
+  vpc_id = aws_vpc.pet-clinic-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.pet-clinic-ig.id
+  }
+  tags = {
+    Name    = "pet-clinic-public"
+    Creator = "Sukresh"
+  }
+}
+
+resource "aws_route_table_association" "pet-clinic-public-subnet-association" {
+  route_table_id = aws_route_table.pet-clinic-public.id
+  subnet_id      = aws_subnet.pet-clinic-public-subnet.id
+}
