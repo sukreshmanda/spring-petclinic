4328427d9d489b4777bd1354c02289d7dc32a8bb
sukreshmanda <mandasukresh@gmail.com>
2023-06-08 10:10:02 +0530

    Add multiple NAT gateways for AZ

--- iac/private.tf
+++ iac/private.tf
@@ -30,27 +30,30 @@ resource "aws_eip" "pet-clinic-nat-eip" {
 }
 
 resource "aws_nat_gateway" "pet-clinic-nat-gateway" {
-  subnet_id     = aws_subnet.pet-clinic-public-subnet["0"].id
+  for_each      = aws_subnet.pet-clinic-public-subnet
+  subnet_id     = each.value.id
   allocation_id = aws_eip.pet-clinic-nat-eip.id
   depends_on    = [
     aws_internet_gateway.pet-clinic-ig
   ]
   tags = {
-    Name    = "pet-clinic-nat-gateway-${aws_subnet.pet-clinic-public-subnet["0"].availability_zone}"
+    Name    = "pet-clinic-nat-gateway-${each.value.availability_zone}"
     Creator = "Sukresh"
   }
 }
 
 resource "aws_route_table" "pet-clinic-private" {
   vpc_id = aws_vpc.pet-clinic-vpc.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.pet-clinic-nat-gateway.id
-  }
-  tags = {
+  tags   = {
     Name    = "pet-clinic-private"
     Creator = "Sukresh"
   }
+}
+resource "aws_route" "pet-clinic-private-route" {
+  for_each               = aws_subnet.pet-clinic-public-subnet
+  route_table_id         = aws_route_table.pet-clinic-private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = each.value.id
 }
 
 resource "aws_route_table_association" "pet-clinic-private-subnet-association" {
